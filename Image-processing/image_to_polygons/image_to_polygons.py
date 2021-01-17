35e505c06e6a0ea5f0f19b5b9524538aea80bd7f
JKKoffi <53520009+JKKoffi@users.noreply.github.com>
2021-01-17 17:07:08 +0000

    Add files via upload

--- Image-processing/image_to_polygons/image_to_polygons.py
+++ Image-processing/image_to_polygons/image_to_polygons.py
@@ -61,6 +61,39 @@ def explode(gdf):
     gdf_out = gdf_out.set_index(['level_0', 'level_1']).set_geometry('geometry')
     gdf_out.crs = gdf.crs
     return gdf_out
+#save also geom of object 
+from collections import defaultdict
+from shapely.geometry import *
+def mask_to_polygons(mask, epsilon=10., min_area=10.):
+    """Convert a mask ndarray (binarized image) to Multipolygons"""
+    # first, find contours with cv2: it's much faster than shapely
+    contours, hierarchy = cv2.findContours(mask,
+                                  cv2.RETR_CCOMP,
+                                  cv2.CHAIN_APPROX_NONE)
+    if not contours:
+        return MultiPolygon()
+    # now messy stuff to associate parent and child contours
+    cnt_children = defaultdict(list)
+    child_contours = set()
+    assert hierarchy.shape[0] == 1
+    # http://docs.opencv.org/3.1.0/d9/d8b/tutorial_py_contours_hierarchy.html
+    for idx, (_, _, _, parent_idx) in enumerate(hierarchy[0]):
+        if parent_idx != -1:
+            child_contours.add(idx)
+            cnt_children[parent_idx].append(contours[idx])
+    # create actual polygons filtering by area (removes artifacts)
+    all_polygons = []
+    for idx, cnt in enumerate(contours):
+        if idx not in child_contours and cv2.contourArea(cnt) >= min_area:
+            assert cnt.shape[1] == 1
+            poly = Polygon(
+                shell=cnt[:, 0, :],
+                holes=[c[:, 0, :] for c in cnt_children.get(idx, [])
+                       if cv2.contourArea(c) >= min_area])
+            all_polygons.append(poly)
+    all_polygons = MultiPolygon(all_polygons)
+
+    return all_polygons
 
 
 if __name__ == "__main__":
